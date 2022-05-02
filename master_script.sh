9308e553f687201f958528b18973980ba2f38150
ohenaoz <ohenaoz@eafit.edu.co>
2022-05-01 22:51:31 -0500

    requirements and bash script

---
+++ master_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+ACCESS_KEY_ID = 'AKIAYBSFEO5WE6WXM5G6'
+SECRET_ACCESS_KEY = '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'
+AWS_DEFAULT_REGION = 'us-east-1'
+
+aws configure set aws_access_key_id $ACCESS_KEY_ID
+aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
+python ahenao_web_app.py
